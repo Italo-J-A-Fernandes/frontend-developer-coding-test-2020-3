4510908affdd80d5c95d85c330ad6b61cecf5bc1
Italo Fernandes <italojafernandes@hotmail.com>
2020-11-19 19:15:42 -0300

    created route, initial layout page not found

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import {} from './styled';
+
+const NotFound: React.FC = () => {
+  return <h1>Not Found</h1>;
+};
+
+export default NotFound;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,13 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import RestaurantsList from '../pages/RestaurantsList/index';
 import RestaurantDetail from '../pages/RestaurantDetail/index';
+import NotFound from '../pages/NotFound/index';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route exact path="/" component={RestaurantsList} />
     <Route path="/home" component={RestaurantsList} />
-    <Route path="/restaurants-list" component={RestaurantsList} />
     <Route path="/restaurant-detail" component={RestaurantDetail} />
+    <Route component={NotFound} />
   </Switch>
 );
 
