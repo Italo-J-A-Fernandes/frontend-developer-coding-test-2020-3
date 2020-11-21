915dcc1f201d7b43c6f59f63adc5c3cccf5e6b4c
Italo Fernandes <italojafernandes@hotmail.com>
2020-11-21 19:33:22 -0300

    displaying restaurant listing

--- src/pages/RestaurantsList/index.tsx
+++ src/pages/RestaurantsList/index.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Header, ContainerListRest } from './styled';
+// import Rater from 'react-rater';
+import ReactStars from 'react-rating-stars-component';
 
+import { Header, ContainerListRest, ListRestaurants, RestItem } from './styled';
 import client from '../../services/apollo';
-import api from '../../services/api';
 import { LIST_RESTAURANTS } from '../../services/graphql-querys';
 
 interface Restaurant {
@@ -18,23 +19,24 @@ interface Restaurant {
       is_open_now: boolean;
     },
   ];
-  photos: [];
+  photos: string;
   price: string;
   rating: number;
 }
 
 const RestaurantsList: React.FC = () => {
   const [restaurants, setRestaurants] = useState<Restaurant[] | null>(null);
+  const wd = window.innerWidth;
 
   useEffect(() => {
-    // client
-    //   .query({
-    //     query: LIST_RESTAURANTS,
-    //     variables: { categories: '', price: '' },
-    //   })
-    //   .then(response => {
-    //     setRestaurants(response.data.search.business);
-    //   });
+    client
+      .query({
+        query: LIST_RESTAURANTS,
+        variables: { categories: '', price: '' },
+      })
+      .then(response => {
+        setRestaurants(response.data.search.business);
+      });
   }, []);
 
   return (
@@ -49,7 +51,52 @@ const RestaurantsList: React.FC = () => {
       <main>
         <ContainerListRest>
           <h2>All Restaurants</h2>
+          <ListRestaurants>
+            {restaurants &&
+              restaurants.map(restaurant => (
+                <RestItem
+                  key={restaurant.id}
+                  img={restaurant.photos}
+                  open={
+                    restaurant.hours[0]
+                      ? restaurant.hours[0].is_open_now
+                      : false
+                  }
+                >
+                  <div className="imagem" />
+                  <div className="infoRest">
+                    <h3>{restaurant.name}</h3>
+                    <ReactStars
+                      activeColor="#002b56e0"
+                      isHalf
+                      edit={false}
+                      value={2.5}
+                    />
+                    {/* <Rater total={5} rating={2.5} interactive={false} /> */}
+                    <div className="restStatus">
+                      <span>
+                        {`${restaurant.categories[0].title} - ${restaurant.price}`}
+                      </span>
+                      {wd <= 425 ? (
+                        <span className="status">
+                          {restaurant.hours[0].is_open_now ? 'open' : 'closed'}
+                        </span>
+                      ) : (
+                        <span className="status">
+                          {restaurant.hours[0].is_open_now
+                            ? 'open now'
+                            : 'closed'}
+                        </span>
+                      )}
+                    </div>
 
+                    <button type="button">
+                      <span>Learn More</span>
+                    </button>
+                  </div>
+                </RestItem>
+              ))}
+          </ListRestaurants>
           <div>
             <button type="button">Load More</button>
           </div>
--- src/pages/RestaurantsList/styled.ts
+++ src/pages/RestaurantsList/styled.ts
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
+interface RestItemProps {
+  img: string;
+  open: boolean;
+}
+
 export const Header = styled.header`
   display: flex;
   flex-direction: column;
@@ -24,7 +29,7 @@ export const Header = styled.header`
     line-height: 32px;
   }
 
-  @media only screen and (min-width: 432px) {
+  @media only screen and (min-width: 504px) {
     padding: 36px 64px 0;
 
     > h1 {
@@ -55,6 +60,7 @@ export const ContainerListRest = styled.section`
   > div {
     display: flex;
     justify-content: center;
+    margin-top: 48px;
 
     > button {
       font-family: Helvetica-Medium;
@@ -80,7 +86,7 @@ export const ContainerListRest = styled.section`
       }
     }
   }
-  @media only screen and (min-width: 432px) {
+  @media only screen and (min-width: 504px) {
     padding: 64px 64px 80px;
 
     > h2 {
@@ -90,6 +96,8 @@ export const ContainerListRest = styled.section`
     }
 
     > div {
+      margin-top: 0;
+
       > button {
         padding: 15px 167px 17px;
         background: #fff;
@@ -102,6 +110,213 @@ export const ContainerListRest = styled.section`
           color: #fff;
         }
       }
+    }
+  }
+`;
+
+export const ListRestaurants = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+
+  li:nth-child(4n) {
+    margin-right: 0;
+  }
+`;
+
+export const RestItem = styled.li<RestItemProps>`
+  display: flex;
+  flex-direction: column;
+  width: 23%;
+  margin: 0 2.6% 6% 0;
+  transition: box-shadow linear 0.2s;
+  border: 1px solid #d4d2d2;
+  padding: 10px;
+  border-radius: 4px;
+
+  :hover {
+    box-shadow: 3px 6px 20px #888888;
+  }
+  .imagem {
+    width: 100%;
+    height: 228px;
+    background-image: url(${props => `${props.img}`});
+    background-repeat: no-repeat;
+    background-size: cover;
+    margin-bottom: 16px;
+  }
+
+  .infoRest {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+  }
+
+  h3 {
+    font-family: Helvetica-Nue;
+    font-size: 19px;
+    font-size: 0.95rem;
+    color: #000000;
+    letter-spacing: 1px;
+    line-height: 28px;
+    margin-bottom: 8px;
+    flex-grow: 1;
+  }
+
+  .react-rater {
+    display: flex;
+    font-size: 1rem;
+    margin-bottom: 16px;
+    /* flex-grow: 1; */
+  }
+  .react-rater-star.is-disabled.is-active {
+    color: #002b56e0;
+  }
+  .react-rater-star.is-disabled{
+    color: #d4d4d4;
+  }
+  .react-rater-star.is-active-half:before {
+    color: #002b56e0;
+  }
+
+  .restStatus {
+    display: flex;
+    justify-content: space-between;
+    /* flex-grow: 1; */
+    flex-wrap: wrap;
+    margin-bottom: 20px;
+
+    > span {
+      font-family: Helvetica-Nue;
+      font-size: 12px;
+      font-size: 0.6rem;
+      color: #757575;
+      letter-spacing: 0.5px;
+      line-height: 16px;
+      text-transform: uppercase;
+    }
+
+    > .status::before {
+      content: '';
+      background: ${props => (props.open ? '#00E8A4' : '#ff3548')};
+      width: 8px;
+      height: 8px;
+      display: inline-block;
+      margin-right: 5px;
+      border-radius: 50%;
+    }
+  }
+
+  button {
+    padding: 16px 0;
+    font-family: Helvetica-Nue;
+    font-size: 14px;
+    font-size: 0.7rem;
+    color: #ffffff;
+    letter-spacing: 1px;
+    text-align: center;
+    line-height: 16px;
+    background: #002b56;
+    border: none;
+    border-radius: 2px;
+    transition: background linear 0.2s;
+
+    :hover {
+      background: #002b56e0;
+    }
+  }
+
+  @media only screen and (min-width: 425px) and (max-width: 1440px) {
+    .imagem {
+      height: 150px;
+    }
+    button {
+      padding: 12px 0;
+    }
+  }
+
+  @media only screen and (min-width: 426px) and (max-width: 1024px) {
+    h3 {
+      font-size: 20px;
+      font-size: 1rem;
+      width: 180px;
+    }
+    .react-rater {
+      margin-bottom: 8px;
+    }
+  }
+
+  @media only screen and (max-width: 425px) {
+    flex-direction: row;
+    width: 100%;
+    margin: 0;
+
+    & + li {
+      margin-top: 32px;
+    }
+
+    h3 {
+      text-overflow: ellipsis;
+      overflow: hidden;
+      height: 30px;
+      white-space: nowrap;
+      margin-top: -7px;
+    }
+
+    .imagem {
+      width: 38%;
+      height: 132px;
+      margin-bottom: 0px;
+    }
+
+    .react-rater {
+      margin-bottom: 8px;
+    }
+    .restStatus {
+      margin-bottom: 15px;
+
+      .status {
+        background: ${props => (props.open ? '#00E8A4' : '#ff3548')};
+        border-radius: 8px;
+        padding: 0px 5px;
+        color: ${props => (props.open ? '#002b56' : '#fff')};
+      }
+
+      > .status::before {
+        display: none;
+      }
+    }
+
+    .infoRest {
+      width: 60%;
+      margin-left: 12px;
+    }
+
+    button {
+    padding: 0;
+    font-size: 16px;
+    font-size: 0.8rem;
+    color: #002B56;
+    letter-spacing: 1px;
+    text-align: left;
+    line-height: 16px;
+    background: transparent;
+    border: none;
+    transition: background linear 0.2s;
+
+    > span{
+      border-bottom: 1px #E6E6E6 solid;
+
+      ::after{
+        content: "→";
+        font-size: 25px;
+        width: 20px;
+        height: 20px;
+      }
+    }
+
+    :hover {
+      background: transparent;
     }
   }
 `;
